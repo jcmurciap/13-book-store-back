f6d59823a27a179f45301385a97b5060334c1897
enigma <camilo.murcia.pimentel@gmail.com>
2021-11-09 21:40:35 -0500

    Add new event model skeleton

---
+++ models/events.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+// Info que se grabará en la base de datos
+const EventSchema = Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    author: {
+        type: String,
+        require: true,
+    },
+    price: {
+        type: Number,
+        require: true,
+    },
+    user: {
+        // es una referencia a user
+        type: Schema.Types.ObjectId,
+        ref: 'Model',
+    },
+}); 
